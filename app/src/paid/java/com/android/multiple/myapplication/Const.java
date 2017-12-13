f7e33c3c3de61936853b8bad4b3df8070ff2349b
revadox <vdbazinga79@gmail.com>
2017-12-13 23:52:17 +0530

    Initial commit

---
+++ app/src/paid/java/com/android/multiple/myapplication/Const.java
@@ -0,0 +1,13 @@
+package com.android.multiple.myapplication;
+
+/**
+ * Created by Acers on 13-12-2017.
+ */
+
+public class Const {
+    public enum Type {
+        FREE, PAID;
+    }
+
+    public static final Type type = Type.PAID;
+}
